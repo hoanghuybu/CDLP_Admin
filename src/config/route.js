12c1faf90394120c77705d0eb7a4ef37e76530aa
hoanghuybu <dohoanghuy8a3@gmail.com>
2023-10-19 18:07:56 +0700

    first commit

---
+++ src/config/route.js
@@ -0,0 +1,10 @@
+const route = {
+    home: '/',
+    following: '/following',
+    profile: '/:nickname',
+    upload: '/upload',
+    search: '/search',
+    live: '/live',
+};
+
+export default route;
